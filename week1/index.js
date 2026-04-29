53024b6fe38c2061bf15899bd4408438a97a41b3
정서윤 <se0yunn@naver.com>
2026-04-29 07:34:12 +0000

    week 1 exercise

--- week1/index.js
+++ week1/index.js
@@ -3,9 +3,13 @@ const express = require("express");
 const app = express();
 
 app.get("/hello", (req, res) => {
-    res.send("Hello world!");
+    res.send("Hello, World!");
+});
+
+app.get("/bye", (req, res) => {
+    res.send("Goodbye, World!");
 });
 
 app.listen(8080, () => {
-    console.log('Server listening on port 3000!');
+    console.log('Server listening on port 8080!');
 });
